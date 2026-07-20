473f9531a4681a65440613f00d594e69200ed1b2
hallucinogen <hallucinogen@localhost>
2026-07-20 04:20:17 +0000

    chore: migrate vendored → referenced (set-and-setting standard)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Subscribe nix flake repos to centralized nixpkgs pin via nixpkgs-lock";
+  description = "CHANGEME";
 
   nixConfig = {
     extra-substituters = [ "https://pr0d1r2.cachix.org" ];
@@ -7,20 +7,25 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-lock.url = "github:pr0d1r2/nixpkgs-lock";
+    nixpkgs.follows = "nixpkgs-lock/nixpkgs";
+
+    set-and-setting.url = "github:pr0d1r2/set-and-setting";
+
     nix-dev-shell-agentic = {
       url = "github:pr0d1r2/nix-dev-shell-agentic";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
+      };
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      set-and-setting,
       nix-dev-shell-agentic,
       ...
-    }@inputs:
+    }:
     let
       supportedSystems = [
         "aarch64-darwin"
@@ -30,38 +35,84 @@
       ];
       forAllSystems =
         f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+
+      fragments = [
+        "base"
+        "nix"
+        "shell"
+        "ascii"
+        "markdown"
+        "yaml"
+      ];
     in
     {
       packages = forAllSystems (pkgs: {
-        default = pkgs.writeShellApplication {
-          name = "nixpkgs-lock-subscribe";
-          runtimeInputs = [
-            pkgs.git
-            pkgs.gh
-            pkgs.coreutils
-            pkgs.gnused
-            pkgs.gnugrep
-            pkgs.jq
-          ];
-          text = builtins.readFile ./nixpkgs-lock-subscribe.sh;
-        };
+        setting = (set-and-setting.lib.mkSetting { inherit pkgs; }).materialized;
       });
 
       devShells = forAllSystems (
         pkgs:
         let
-          inherit (pkgs.stdenv.hostPlatform) system;
-          shells = nix-dev-shell-agentic.lib.mkShells {
-            inherit pkgs inputs;
-            ciPackages = [
-              self.packages.${system}.default
-            ];
-            shellHook = builtins.replaceStrings [ "@BATS_LIB_PATH@" ] [ "${shells.batsWithLibs}" ] (
-              builtins.readFile ./dev.sh
-            );
-          };
+          mat = set-and-setting.lib.materializationFor { inherit pkgs fragments; };
+          sys = pkgs.stdenv.hostPlatform.system;
         in
-        shells
+        set-and-setting.lib.mkDevShells {
+          inherit pkgs;
+          basePackages = mat.packages;
+          settingHook = ''
+            ${self.packages.${sys}.setting}/bin/sync-setting .
+            _assemble_out="$(mktemp -d)"
+            FRAGMENTS="${builtins.concatStringsSep " " fragments}" \
+              out="$_assemble_out" \
+              FRAGMENTS_DIR="${set-and-setting}/setting/integrations/lefthook" \
+              bash "${set-and-setting}/setting/lib/assemble-lefthook.sh"
+            cp -f "$_assemble_out/lefthook.yml" lefthook.yml
+            rm -rf "$_assemble_out"
+          '';
+        }
       );
+
+      checks = forAllSystems (
+        pkgs:
+        (set-and-setting.lib.checksFor {
+          inherit pkgs fragments;
+          src = ./.;
+        })
+        // {
+          dep-graph = set-and-setting.lib.mkDepGraphCheck {
+            inherit pkgs;
+            projectRoot = ./.;
+          };
+          default = pkgs.runCommand "checks" { } "touch $out";
+        }
+      );
+
+      apps = forAllSystems (pkgs: {
+        confirm = {
+          type = "app";
+          program = "${
+            pkgs.writeShellApplication {
+              name = "confirm";
+              runtimeInputs = [
+                pkgs.coreutils
+                pkgs.diffutils
+                pkgs.findutils
+                pkgs.gawk
+                pkgs.git
+                pkgs.gnugrep
+              ];
+              text = ''
+                export FRAGMENTS_DIR="${set-and-setting}/setting/integrations/lefthook"
+                export ASSEMBLE_SCRIPT="${set-and-setting}/setting/lib/assemble-lefthook.sh"
+                export DETECT_SCRIPT="${set-and-setting}/setting/lib/detect-fragments.sh"
+                export SETTING_SRC="${self.packages.${pkgs.stdenv.hostPlatform.system}.setting}"
+                export CONFIRM_SCRIPT="${set-and-setting}/lib/confirm.sh"
+                export CONFIRM_REV="${set-and-setting.rev or "unknown"}"
+                bash "$CONFIRM_SCRIPT"
+              '';
+            }
+          }/bin/confirm";
+        };
+      });
     };
 }
